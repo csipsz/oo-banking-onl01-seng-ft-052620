41c1d6ebb41e592fbcc8bbbf6d6989245efb2129
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 15:59:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
     if status == 'complete'
       receiver.balance -= amount 
       sender.balance += amount 
-      @status = 'reversed'
+      status = 'reversed'
       
   end 
 end 
