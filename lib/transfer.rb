fea1eea0e4d1de26644e2baa1ab5e916e2877832
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 16:25:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,25 +11,25 @@ class Transfer
   end 
   
   def valid? 
-    @sender.valid? && @receiver.valid? 
+    sender.valid? && receiver.valid? 
   end 
   
   def execute_transaction 
-    if valid? && status == 'pending' && sender.balance > amount
+    if valid? && @status == 'pending' && sender.balance > amount
       receiver.balance += amount
       sender.balance -= amount
-      status = 'complete'
+      @status = 'complete'
     else 
-    status = 'rejected'
+    @status = 'rejected'
     return "Transaction rejected. Please check your account balance."
   end 
   end 
 
   def reverse_transfer
-    if status == 'complete'
+    if @status == 'complete'
       receiver.balance -= amount 
       sender.balance += amount 
-      status = 'reversed'
+      @status = 'reversed'
       
   end 
 end 
