49a0c0c742f255b715169a0c6116d60de45d9532
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 16:09:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,4 @@
 require 'pry'
-=begin
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
@@ -35,40 +34,4 @@ class Transfer
   end 
 end 
   
-end
-
-=end 
-
-class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
-
-  def valid?
-    @sender.valid? && @receiver.valid? ? true : false
-  end
-
-  def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
-  end
-
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
-    end
-  end
 end
