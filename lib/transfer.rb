f270fdd7d4e084c06025e0fd63ad14c9f08da9a3
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 16:15:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,6 @@ class Transfer
     @sender.valid? && @receiver.valid? 
   end 
   
-=begin
   def execute_transaction 
     if valid? && @status == 'pending'
       @receiver.balance += @amount
@@ -25,24 +24,7 @@ class Transfer
     return "Transaction rejected. Please check your account balance."
   end 
   end 
-=end 
 
-def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
-  end
-
-
-  
   def reverse_transfer
     if @status == 'complete'
       receiver.balance -= amount 
