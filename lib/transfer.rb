65796e0280d608d0727b2bafae6aa7b4d587418f
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 15:29:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if !sender.valid?
+    if (!sender.valid?)
       self.status = 'rejected'
       "Transaction rejected. Please check your account balance."
     else 
